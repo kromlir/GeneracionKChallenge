ea6ab17925e304178a9d82792bc11deaa55fdc60
Manel Alonso <macalonspc@gmail.com>
2021-06-30 19:33:18 +0200

    Finished form control implementation, started work on programing of the forms.

--- RetoGeneracionK/Main.cs
+++ RetoGeneracionK/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,20 @@ namespace RetoGeneracionK
 {
     public partial class Main : Form
     {
+        decrypterEngine toDecrypt = new decrypterEngine();
         public Main()
         {
             InitializeComponent();
+            
+        }
+        private void buttonFileDecrypt_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogDecrypt.ShowDialog() == DialogResult.OK)
+            {
+                
+
+                toDecrypt.DataToDecrypt = ;
+            }
         }
     }
 }
